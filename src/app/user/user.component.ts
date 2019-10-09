d2e90f415b933e08111de0aa7cbacb73f9b2f738
A9605336 <anassyakine06@gmail.com>
2019-10-09 11:31:58 +0200

    add template with the steppers

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Stepper from 'bs-stepper';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  private stepper: Stepper;
 
   constructor() { }
 
+  next() {
+    this.stepper.next();
+  }
+  onSubmit() {
+    return false;
+  }
+
   ngOnInit() {
+    this.stepper = new Stepper(document.querySelector('#stepper1'), {
+      linear: false,
+      animation: true
+    })
   }
 
 }
