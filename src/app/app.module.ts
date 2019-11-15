083251dc3dcb37a22d652a40260e1ae6da230089
Mitrail <edouardguegain@gmail.com>
2019-11-16 00:35:04 +0100

    FIX : delete mdbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +9,6 @@ import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
 import { ProfilComponent } from './user/components/profil/profil.component';
 import { NotifComponent } from './user/components/notif/notif.component';
-import { FilterComponent } from './user/components/filter/filter.component';
 import {GlobalConfigurationService} from './shared/services/globalConfiguration.service';
 import {CompareItAPIService} from './shared/services/compareItAPI.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -18,6 +16,8 @@ import { LoginComponent } from './login/login.component';
 import { ModelComponent } from './admin/components/model/model.component';
 import { ModelpropertyComponent } from './admin/components/model-property/model-property.component';
 import {BasicAuthInterceptor} from './_helpers/basic-auth.interceptor';
+import { FileSelectDirective } from 'ng2-file-upload';
+import {SliderModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
@@ -28,7 +28,7 @@ import {BasicAuthInterceptor} from './_helpers/basic-auth.interceptor';
     AdminComponent,
     ProfilComponent,
     NotifComponent,
-    FilterComponent,
+    FileSelectDirective,
     LoginComponent,
     ModelComponent,
     ModelpropertyComponent
@@ -38,8 +38,8 @@ import {BasicAuthInterceptor} from './_helpers/basic-auth.interceptor';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
-    MDBBootstrapModule.forRoot()
+    SliderModule,
+    ReactiveFormsModule
   ],
   providers: [
     GlobalConfigurationService,
