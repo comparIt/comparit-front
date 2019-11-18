9497aff2850df22693783fba884d087b6ee281e5
Mitrail <edouardguegain@gmail.com>
2019-11-18 17:24:33 +0100

    WIP : template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 import {HeaderComponent} from './shared/components/header/header.component';
-import { UserComponent } from './user/user.component';
-import { AdminComponent } from './admin/admin.component';
-import { ProfilComponent } from './user/components/profil/profil.component';
-import { NotifComponent } from './user/components/notif/notif.component';
+import {UserComponent} from './user/user.component';
+import {AdminComponent} from './admin/admin.component';
+import {ProfilComponent} from './user/components/profil/profil.component';
+import {NotifComponent} from './user/components/notif/notif.component';
 import {GlobalConfigurationService} from './shared/services/globalConfiguration.service';
 import {CompareItAPIService} from './shared/services/compareItAPI.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { LoginComponent } from './login/login.component';
-import { ModelComponent } from './admin/components/model/model.component';
+import {LoginComponent} from './login/login.component';
+import {ModelComponent} from './admin/components/model/model.component';
 import {BasicAuthInterceptor} from './_helpers/basic-auth.interceptor';
-import { FileSelectDirective } from 'ng2-file-upload';
+import {FileSelectDirective} from 'ng2-file-upload';
 import {SliderModule} from 'primeng/primeng';
-import { ProductComponent } from './product/product.component';
+import {ProductComponent} from './product/product.component';
 import {ResumeProductComponent} from './product/resume-product/resume-product.component';
 
 @NgModule({
@@ -45,12 +45,13 @@ import {ResumeProductComponent} from './product/resume-product/resume-product.co
   ],
   providers: [
     GlobalConfigurationService,
-      {
-          provide: HTTP_INTERCEPTORS,
-          useClass: BasicAuthInterceptor,
-          multi: true
-      },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: BasicAuthInterceptor,
+      multi: true
+    },
     CompareItAPIService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
