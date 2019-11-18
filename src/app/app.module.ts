3b33784b3ed49b63bd9bdb01cbfa4c4a6c99e720
Mathieu <msansen@gmail.com>
2019-11-18 16:40:14 +0100

    WIP rebase from develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent} from './shared/components/header/header.component';
+import {HeaderComponent} from './shared/components/header/header.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
 import { ProfilComponent } from './user/components/profil/profil.component';
 import { NotifComponent } from './user/components/notif/notif.component';
-import { SupplierComponent } from './supplier/supplier.component';
 import {GlobalConfigurationService} from './shared/services/globalConfiguration.service';
 import {CompareItAPIService} from './shared/services/compareItAPI.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -19,6 +19,7 @@ import {BasicAuthInterceptor} from './_helpers/basic-auth.interceptor';
 import { FileSelectDirective } from 'ng2-file-upload';
 import {SliderModule} from 'primeng/primeng';
 import { UploadComponent } from './upload/upload.component';
+import { SupplierComponent } from './supplier/supplier.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import { UploadComponent } from './upload/upload.component';
     FileSelectDirective,
     LoginComponent,
     ModelComponent,
-    ModelpropertyComponent
+    ModelpropertyComponent,
     SupplierComponent,
     UploadComponent
   ],
