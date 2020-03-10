d9218070b7d54c55a9b9ef48dbac7b3d3a18b8a6
Mathieu <msansen@gmail.com>
2020-03-10 18:27:23 +0100

    fix2: problem with settings used...make front image configurable with a settings file instead of a build image with hard links to backend API

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://demo.comparit.fr/api',
-  analyticsUrl: 'https://analytics.comparit.fr/',
-  analyticsId: 1,
-  SiteTitle: 'compare-it-front'
-};
--- src/environments/environment.staging.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://compit.ddns.net:8080',
-  analyticsUrl: 'https://analytics.comparit.fr/',
-  analyticsId: 1,
-  SiteTitle: 'compare-it-front'
-};
